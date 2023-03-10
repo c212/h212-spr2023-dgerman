206ea7eadb6a4c8c1b7b6f3b9703adcced7cf504
c212 <40395648+c212@users.noreply.github.com>
2023-03-10 15:24:42 -0500

    Create Displaux.java

---
+++ fri-mar-10/Displaux.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList; 
+
+public class Displaux {
+  public ArrayList<String> lines;
+  public int width, height, middle; 
+  public Displaux(ArrayList<String> lines, int width, int height, int middle) {
+    this.lines = lines; 
+    this.width = width; 
+    this.height = height;
+    this.middle = middle; 
+  }
+}
